a7b80536ed7f9b55e9b41c9094862fcbaf4840b7
AstroShen <768299856@qq.com>
2023-08-22 23:01:29 +0800

    add sniprun plugin

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,9 +3,10 @@ local utils = require "astronvim.utils"
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.bash" },
-  { import = "astrocommunity.pack.cpp" },
+  -- { import = "astrocommunity.pack.cpp", enabled = false },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.lua", enabled = false },
   { import = "astrocommunity.editing-support.zen-mode-nvim" },
   { import = "astrocommunity.bars-and-lines.lualine-nvim" },
+  { import = "astrocommunity.code-runner.sniprun" },
 }
