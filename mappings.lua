f6fe21f7ecfb9b30a067a5638a520ca23f147c5b
AstroShen <768299856@qq.com>
2023-08-27 14:30:12 +0800

    add quickfix mapping

--- mappings.lua
+++ mappings.lua
@@ -13,18 +13,20 @@ return {
     ["<leader>w"] = { ":update<cr>", desc = "update file" },
     ["]c"] = { ":cnext<cr>", desc = "next quickfix item" },
     ["[c"] = { ":cprevious<cr>", desc = "previous quickfix item" },
-    ["<tab>"] = {
+    ["L"] = {
       function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
       desc = "Next buffer",
     },
-    ["<s-tab>"] = {
+    ["H"] = {
       function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
       desc = "Previous buffer",
     },
+    ["<leader>q"] = { ":bot copen<cr>", desc = "open/goto quickfix" },
     ["<leader>s"] = { ":ClangdSwitchSourceHeader<cr>", desc = "siwtch source and header file" },
     ["<leader>m"] = { ":AsyncRun -program=make<cr>", desc = "Run make program async" },
     ["<leader>M"] = { ":AsyncRun -program=make && %:p:r<cr>", desc = "Run make program and execute async" },
     ["gh"] = { ":help <c-r><c-w><cr>", desc = "help word under cursor" },
+    ["<c-'>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
   },
   i = {
     ["<c-e>"] = { "<end>", desc = "move to last of the line in insert mode" },
